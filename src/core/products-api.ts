3318ceb9f1a34ca658dd5bb699a17c329215f867
MD. MOHIBUR RAHMAN <35300157+mrpmohiburrahman@users.noreply.github.com>
2024-11-05 15:51:54 +0600

    ✨ api for getting product

---
+++ src/core/products-api.ts
@@ -0,0 +1,27 @@
+// src/features/products/productsApi.ts
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export interface Product {
+  id: number;
+  title: string;
+  price: number;
+  description: string;
+  category: string;
+  image: string;
+}
+
+export const productsApi = createApi({
+  reducerPath: 'productsApi',
+  keepUnusedDataFor: 60 * 60, // 1 hour
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://fakestoreapi.com/' }),
+  endpoints: (builder) => ({
+    getProducts: builder.query<Product[], void>({
+      query: () => 'products',
+    }),
+    getProductById: builder.query<Product, number>({
+      query: (id) => `products/${id}`,
+    }),
+  }),
+});
+
+export const { useGetProductsQuery, useGetProductByIdQuery } = productsApi;
